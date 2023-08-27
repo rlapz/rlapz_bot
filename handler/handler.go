4cf1ffbf4ba98cd74083f7bf9018652a646e302a
Arthur Lapz <rlapz@gnuweeb.org>
2023-08-28 01:47:17 +0700

    HandleFn: test: parse telegram incoming update

--- handler/handler.go
+++ handler/handler.go
@@ -1,11 +1,24 @@
 package handler
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
 	"net/http"
+
+	"github.com/rlapz/rlapz_bot/types"
 )
 
 func HandleFn(r *http.Request, url string) {
-	fmt.Println(r.URL.Path)
-	fmt.Println(r.Body)
+	var res types.Update
+	if err := json.NewDecoder(r.Body).Decode(&res); err != nil {
+		log.Println("HandleFn: ", err.Error())
+		return
+	}
+
+	if res.UpdateId == 0 {
+		log.Println("HandleFn: invalid update id")
+		return
+	}
+
+	log.Println("text: ", res.Message.Text)
 }
